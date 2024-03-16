b7b9cbac3ea46d450657ee5cd1fe4e81bf48f7b6
Sujeong Ji <suji.sujeongji@gmail.com>
2024-03-16 23:59:48 +0900

    feat: implement titleSlice and integrate with TitleContainer for title inputs and focus management

---
+++ src/redux/reducers/titleSlice.ts
@@ -0,0 +1,35 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+interface ITitleProps {
+  title: string;
+  desc: string;
+  isFocused: boolean;
+}
+
+const initialState: ITitleProps = {
+  title: "제목 없는 설문지",
+  desc: "설문지 설명",
+  isFocused: true,
+};
+
+export const titleSlice = createSlice({
+  name: "title",
+  initialState,
+  reducers: {
+    setTitle: (state, action) => {
+      const { title } = action.payload;
+      state.title = title;
+    },
+    setDesc: (state, action) => {
+      const { desc } = action.payload;
+      state.desc = desc;
+    },
+    setIsTitleFocused(state, action) {
+      state.isFocused = action.payload;
+    },
+  },
+});
+
+export const { setTitle, setDesc, setIsTitleFocused } = titleSlice.actions;
+
+export default titleSlice.reducer;
